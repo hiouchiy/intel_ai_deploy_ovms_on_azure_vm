a672182e8805afb9e65b611cf89f3716870e6c9f
houchiya <hiroshi.ouchiyama@intel.com>
2021-01-01 14:17:49 +0900

    Update

--- setup_ovms.sh
+++ setup_ovms.sh
@@ -1,4 +1,9 @@
-#/bin/sh
+#!/bin/sh
+
+PORT_NUMBER=$1
+MODEL_NAME=$2
+MODEL_PATH=$3
+AZURE_STORAGE_CONNECTION_STRING="SharedAccessSignature=sv=2018-03-28&ss=b&srt=sco&sp=rl&st=2021-01-01T00%3A55%3A39Z&se=2022-01-01T00%3A55%3A00Z&sig=qA38onoiJMjX3dIDOBnELWTs2yNjNDQNtnRb1vz5068%3D;BlobEndpoint=https://ovaasstorage.blob.core.windows.net/;"
 
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
@@ -8,4 +13,5 @@ sudo apt update
 apt-cache policy docker-ce
 sudo apt install -y docker-ce
 sudo usermod -aG docker ${USER}
-sudo docker run --rm -d -p 9000:9000 -e AZURE_STORAGE_CONNECTION_STRING="DefaultEndpointsProtocol=https;AccountName=ovaasstorage;AccountKey=1+htUw4UA7P0h5lUfiYZcqJNJYmM+HMONOEDrhhWGApJXWoch95lmPG4ub277+PEGv4wYNkSWrlrUwJqPEn1Vg==;EndpointSuffix=core.windows.net" openvino/model_server:latest --model_path az://ovms/intel/human-pose-estimation-0001/FP16-INT8 --model_name human-pose-estimation --port 9000
+#sudo docker run --rm -d -p 9000:9000 -e AZURE_STORAGE_CONNECTION_STRING="SharedAccessSignature=sv=2018-03-28&ss=b&srt=sco&sp=rl&st=2021-01-01T00%3A55%3A39Z&se=2022-01-01T00%3A55%3A00Z&sig=qA38onoiJMjX3dIDOBnELWTs2yNjNDQNtnRb1vz5068%3D;BlobEndpoint=https://ovaasstorage.blob.core.windows.net/;" openvino/model_server:latest --model_path az://ovms/intel/human-pose-estimation-0001/FP16-INT8 --model_name human-pose-estimation --port 9000
+sudo docker run --rm -d -p $PORT_NUMBER:$PORT_NUMBER -e AZURE_STORAGE_CONNECTION_STRING=$AZURE_STORAGE_CONNECTION_STRING openvino/model_server:latest --model_path $MODEL_PATH --model_name $MODEL_NAME --port $PORT_NUMBER
