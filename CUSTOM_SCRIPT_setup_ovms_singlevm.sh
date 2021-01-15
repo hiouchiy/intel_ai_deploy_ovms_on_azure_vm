79bdbb203e8a58df3fcbd53a6fbb379f78bd422e
houchiya <hiroshi.ouchiyama@intel.com>
2021-01-15 17:07:26 +0900

    Update

---
+++ CUSTOM_SCRIPT_setup_ovms_singlevm.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+AZURE_STORAGE_CONNECTION_STRING=$1
+
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install -y docker-ce
+sudo usermod -aG docker ${USER}
+
+for i in `seq 1 ${#}`
+do
+
+    if [ ${i} -eq 1 ]; then
+        shift
+        continue
+    fi
+    
+    PORT_NUMBER=`echo ${1} | cut -d , -f 1`
+    MODEL_NAME=`echo ${1} | cut -d , -f 2`
+    MODEL_PATH=`echo ${1} | cut -d , -f 3`
+    echo "$PORT_NUMBER $MODEL_NAME $MODEL_PATH"
+    
+    sudo docker run --rm -d -p $PORT_NUMBER:9000 -e AZURE_STORAGE_CONNECTION_STRING=$AZURE_STORAGE_CONNECTION_STRING openvino/model_server:latest --model_path $MODEL_PATH --model_name $MODEL_NAME --port 9000
+
+    shift
+done
---
+++ deploy_vm_singlevm.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+RESOURCE_GROUP=$2
+LOCATION=japaneast
+
+AF_RESOURCE_GROUP=OVaaS
+AF_NAME=ovaas-backend
+
+ARM_TEMPLATE_FILE=azuredeploy.json
+
+AZURE_STORAGE_CONNECTION_STRING=$3
+
+az group create --name $RESOURCE_GROUP --location $LOCATION
+
+PARAM_FOR_CUSTOM_SCRIPT=""
+
+json=$(cat $1)                                                                                                   
+len=$(echo $json | jq length)                                                                                           
+for i in $( seq 0 $(($len - 1)) ); do                                                                                     
+    echo "Processing $(($i + 1)) row.."                                                                                     
+    row=$(echo $json | jq .[$i])                                                                                            
+    AF_FUNC_NAME=$(echo $row | jq .function_name | sed -e 's/^"//' -e 's/"$//')                                                                          
+    echo $AF_FUNC_NAME
+    MODEL_NAME=$(echo $row | jq .model_name | sed -e 's/^"//' -e 's/"$//')                                                                                
+    echo $MODEL_NAME
+    PORT_NUMBER=$(echo $row | jq .port_number)                                                                              
+    echo $PORT_NUMBER                                                                                                       
+    MODEL_PATH=$(echo $row | jq .model_path_on_azure_storage | sed -e 's/^"//' -e 's/"$//')                                              
+    echo $MODEL_PATH      
+
+    $PARAM_FOR_CUSTOM_SCRIPT="${PARAM_FOR_CUSTOM_SCRIPT} ${PORT_NUMBER},${MODEL_NAME},${MODEL_PATH}"
+done
+
+echo $PARAM_FOR_CUSTOM_SCRIPT
+
+VM_NAME="modelserver"
+echo $VM_NAME
+
+az deployment group create --name ExampleDeployment --resource-group $RESOURCE_GROUP --template-file $ARM_TEMPLATE_FILE --parameters adminUsername=ai adminPasswordOrKey=Passw0rd1234 vmSize=Standard_D2s_v4 authenticationType=password customScriptCommandToExecute="sh CUSTOM_SCRIPT_setup_ovms_singlevm.sh \"${AZURE_STORAGE_CONNECTION_STRING}\" ${PARAM_FOR_CUSTOM_SCRIPT}" vmName=$VM_NAME
+
+IP_ADDRESS=`az vm list-ip-addresses -g $RESOURCE_GROUP -n $VM_NAME -o json | jq -r .[].virtualMachine.network.publicIpAddresses[0].ipAddress`
+
+for i in $( seq 0 $(($len - 1)) ); do                                                                                     
+    echo "Processing $(($i + 1)) row.."                                                                                     
+    row=$(echo $json | jq .[$i])
+    AF_IP_ADDRESS_NAME=$(echo $row | jq .env_name_ip | sed -e 's/^"//' -e 's/"$//')
+    echo $AF_IP_ADDRESS_NAME
+    AF_PORT_NAME=$(echo $row | jq .env_name_port | sed -e 's/^"//' -e 's/"$//')
+    echo $AF_PORT_NAME
+    PORT_NUMBER=$(echo $row | jq .port_number)
+    echo $PORT_NUMBER
+
+    az functionapp config appsettings set --name $AF_NAME  --resource-group $AF_RESOURCE_GROUP --settings "$AF_IP_ADDRESS_NAME=$IP_ADDRESS" "$AF_PORT_NAME=$PORT_NUMBER"
+done
