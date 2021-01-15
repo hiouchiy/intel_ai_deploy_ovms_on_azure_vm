ecc266921eb9d4b74b96c68a6c69f482909ad592
houchiya <hiroshi.ouchiyama@intel.com>
2021-01-15 23:37:57 +0900

    Update

--- deploy_vm.sh
+++ deploy_vm.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 RESOURCE_GROUP=$2
 LOCATION=japaneast
@@ -12,15 +12,13 @@ AZURE_STORAGE_CONNECTION_STRING=$4
 
 az group create --name $RESOURCE_GROUP --location $LOCATION
 
-json=$(cat $1)
-len=$(echo $json | jq length)
-for i in $( seq 0 $(($len - 1)) ); do
+function deploy_vm () {
+    i=$1
+    row=$(echo $json | jq .[$i])
+    echo "Processing $(($i + 1)) row.."
 
     PARAM_FOR_CUSTOM_SCRIPT=""
-
-    echo "Processing $(($i + 1)) row.."
-    row=$(echo $json | jq .[$i])
-
+    
     VM_NAME=$(echo $row | jq .vm_name | sed -e 's/^"//' -e 's/"$//')
     echo $VM_NAME
 
@@ -41,18 +39,52 @@ for i in $( seq 0 $(($len - 1)) ); do
 
     echo $PARAM_FOR_CUSTOM_SCRIPT
 
-    az deployment group create --name ExampleDeployment --resource-group $RESOURCE_GROUP --template-file $ARM_TEMPLATE_FILE --parameters adminUsername=ai adminPasswordOrKey=Passw0rd1234 vmSize=Standard_D2s_v4 authenticationType=password customScriptCommandToExecute="sh CUSTOM_SCRIPT_setup_ovms_singlevm.sh \"${AZURE_STORAGE_CONNECTION_STRING}\" ${PARAM_FOR_CUSTOM_SCRIPT}" vmName=$VM_NAME
+    DEPLOYMENT_NAME="Deployment_${i}"
+    az deployment group create --name $DEPLOYMENT_NAME --resource-group $RESOURCE_GROUP --template-file $ARM_TEMPLATE_FILE --parameters adminUsername=ai adminPasswordOrKey=Passw0rd1234 vmSize=Standard_D2s_v4 authenticationType=password customScriptCommandToExecute="sh CUSTOM_SCRIPT_setup_ovms.sh \"${AZURE_STORAGE_CONNECTION_STRING}\" ${PARAM_FOR_CUSTOM_SCRIPT}" vmName=$VM_NAME
+}
+
+json=$(cat $1)
+len=$(echo $json | jq length)
+for i in $( seq 0 $(($len - 1)) ); do
+    deploy_vm $i &
+    pid_list[${i}]=$!
+    sleep 5
+done
+
+for pid in ${pid_list[@]}; do
+    wait $pid
+    sc=$?
+    if [ 0 -ne $sc ]; then
+        echo "[error] pid=$pid finished with status code $sc"
+#        for pid2 in ${pid_list[@]}; do
+#            kill -KILL $pid2
+#        done
+#        exit 1
+    else
+        echo "[info] pid=$pid finished"
+    fi
+done
+
+echo "[info] -----------------------------"
+echo "[info] finished all background proc."
+
+for i in $( seq 0 $(($len - 1)) ); do
+    row=$(echo $json | jq .[$i])
+    VM_NAME=$(echo $row | jq .vm_name | sed -e 's/^"//' -e 's/"$//')
+    echo $VM_NAME
     
     IP_ADDRESS=`az vm list-ip-addresses -g $RESOURCE_GROUP -n $VM_NAME -o json | jq -r .[].virtualMachine.network.publicIpAddresses[0].ipAddress`
 
+    models=$(echo $row | jq .models)
+    models_len=$(echo $models | jq length)
     for j in $( seq 0 $(($models_len - 1)) ); do
         AF_IP_ADDRESS_NAME=$(echo $models | jq .[$j].env_name_ip | sed -e 's/^"//' -e 's/"$//')
         echo $AF_IP_ADDRESS_NAME
         AF_PORT_NAME=$(echo $models | jq .[$j].env_name_port | sed -e 's/^"//' -e 's/"$//')
         echo $AF_PORT_NAME
         PORT_NUMBER=$(echo $models | jq .[$j].port_number)
-        echo $PORT_NUMBER                                                                      
-                                                                                           
+        echo $PORT_NUMBER
+
         az functionapp config appsettings set --name $AF_NAME  --resource-group $AF_RESOURCE_GROUP --settings "$AF_IP_ADDRESS_NAME=$IP_ADDRESS" "$AF_PORT_NAME=$PORT_NUMBER"
-   done                                                                                                                                                                      
+    done
 done
